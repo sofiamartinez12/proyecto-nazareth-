ff50b63657148fbd2be6a21f9b9ea02ccaa361e7
marisofi <marianajaramillonazareth@gmail.com>
2022-02-28 07:06:42 -0500

    ...

---
+++ Taller1.js
@@ -0,0 +1,12 @@
+//archivo de practica y pruebas de JavaScript
+/* 
+Taller 1
+Fecha 23/02/2022
+Realizado por: marianajaramillo y sofia martinez
+*/
+var nombreEstudiante //string
+var gradoEstudiante="10A" //string
+var edadEstudiante //int
+nombreEstudiante= "mariana y sofia"
+var edadEstudiante= prompt("");
+console.log ("la estudiantes  del grado tienen años "+nombreEstudiante +gradoEstudiante +edadEstudiante );
---
+++ index.js
@@ -0,0 +1,45 @@
+console.log("Hola Mundo");
+// comentarios en linea 
+/*comentarios 
+de
+varias
+lineas
+*/ 
+
+
+var primerVariable;
+let edadUsuario;
+//declarar variables
+var nombreVariable;
+//crear variable sin asignar valor
+/*
+inicializar variables:
+asignar un valor al momento de crear la variable
+*/
+var gradoEstudiante="10A";
+/*
+asignar un valor: 
+ igualar una variable existente a un nuevo valor 
+ */
+edadUsuario=16;
+//asignar valor desde otra variable
+console.log(edadUsuario);
+var edad=17;
+edadUsuario=edad;
+console.log(edadUsuario);
+/*asignar valor desde el teclado: 
+existen dos maneras de hacerlo, la primera es por ventana emergente y la segunda es por medio de un formulario de html 
+*/
+var valor= prompt("");
+console.log("la variable vale "+valor);
+/*
+consonantes:son valores que no pueden ser cambiados durante la ejecucion del codigo. Las constantes siempre deben ser inicializadas 
+*/
+const PI= 3.1416;
+//imprimir por consola
+//simple
+console.log("...");
+console.log(valor);
+//concatenado
+console.log("..."+valor);
+prompt("ingrese un valor:");
