02d476b2f1f9e88bf338a613cac2fe3009089291
marisofi <marianajaramillonazareth@gmail.com>
2022-04-29 11:32:46 -0500

    tallerCondiciones.js

--- tallerCondiciones.js
+++ tallerCondiciones.js
@@ -97,3 +97,4 @@ console.log("es una vocal");
 }else if(caracter=="0" || caracter=="1" || caracter=="2" || caracter=="3" || caracter=="4" || caracter=="5" || caracter=="6" || caracter=="7" || caracter=="8" || caracter=="9"){
 console.log("son numeros");
 }  
+
