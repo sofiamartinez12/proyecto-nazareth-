c232cfd1645ebec93704366f7f5e43024b4aaa3c
marisofi <marianajaramillonazareth@gmail.com>
2022-05-03 13:42:35 -0500

    Evaluacion condicionales

---
+++ evaluacionCondicionales.js
@@ -0,0 +1,63 @@
+//evaluacion
+/*
+Fecha 03/05/2022
+realizada por: Mariana Jaramillo, Sofia Martinez 
+*/
+//solucion punto1
+var ValorVariable= 200;
+ 
+if (ValorVariable < 300 ){
+   console.log("La condición es verdadera")
+}
+
+//solucion punto2
+var valor=10;
+ 
+if (valor >= 60 || valor <= 10){
+   console.log("El valor es correcto")
+}
+ 
+//solucion punto3
+var numero=40;
+ 
+if (numero==="20"){
+   console.log('El número es igual que "20" en valor y tipo');
+}else if(numero>30 && numero<40){
+    console.log('El número es mayor que 30 y menor a 40');
+}else if(numero==30 || numero<=29){
+    console.log("El número es igual que 30 o menor igual que 29");
+}else if(numero>=40){
+    console.log("El número es mayor igual que 40");
+}
+
+//Respuesta 3.A
+numero="20"
+//Respuesta 3.B
+    numero=35
+//Respuesta 3.C
+    numero=30
+//Respuesta 3.D
+    numero=40
+
+//solucion punto4
+var valor1=10;
+var valor2=15;
+if(valor1==valor2){
+console.log("son iguales");
+}else{
+   console.log(valor1+valor2);
+}   
+
+//solucion punto5
+var producto="sandwich";
+if(producto=="sandwich"){
+console.log("el sandwich vale 2500");
+}else if (producto=="churro"){
+console.log("el churro vale 2500");
+}else if (producto=="salchipapa"){
+console.log("la salchipapa vale 2800");
+}else if (producto=="empanada"){
+console.log("la empanada vale 1000");
+}        
+
+
